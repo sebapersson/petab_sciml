0dcf8ba0637ad67604bfb68c29dc4efc6ae18664
sebapersson <sebastian.persson1@gmail.com>
2024-11-11 13:11:12 +0100

    Add Conv2d test-case

---
+++ test_cases/net_004/create_testdata/net.jl
@@ -0,0 +1,32 @@
+using Lux, CSV, DataFrames, StableRNGs, YAML
+
+include(joinpath(@__DIR__, "..", "..", "helper.jl"))
+
+# A Lux.jl Neural-Network model
+nn_model = @compact(
+    layer1 = Conv((5, 5), 1 => 2; cross_correlation = true),
+    layer2 = Conv((5, 2), 2 => 1; cross_correlation = true)
+) do x
+    embed = layer1(x)
+    out = layer2(embed)
+    @return out
+end
+
+for i in 1:3
+    rng = StableRNG(i)
+    ps, st = Lux.setup(rng, nn_model)
+    input = rand(rng, 10, 15, 1, 1)
+    output = nn_model(input, ps, st)[1]
+    df_ps = nn_ps_to_tidy(nn_model, ps, :net)
+    # PyTorch does not need the batch
+    df_input = _array_to_tidy(input[:, :, :, 1]; mapping = [1 => 3, 2 => 1, 3 => 2])
+    df_output = _array_to_tidy(output[:, :, :, 1];  mapping = [1 => 3, 2 => 1, 3 => 2])
+    CSV.write(joinpath(@__DIR__, "..", "net_ps_$i.tsv"), df_ps, delim = '\t')
+    CSV.write(joinpath(@__DIR__, "..", "net_input_$i.tsv"), df_input, delim = '\t')
+    CSV.write(joinpath(@__DIR__, "..", "net_output_$i.tsv"), df_output, delim = '\t')
+end
+solutions = Dict(:net_file => "net.yaml",
+                 :net_ps => ["net_ps_1.tsv", "net_ps_2.tsv", "net_ps_3.tsv"],
+                 :net_input => ["net_input_1.tsv", "net_input_2.tsv", "net_input_3.tsv"],
+                 :net_output => ["net_output_1.tsv", "net_output_2.tsv", "net_output_3.tsv"])
+YAML.write_file(joinpath(@__DIR__, "..", "solutions.yaml"), solutions)
--- test_cases/net_004/create_testdata/net.py
+++ test_cases/net_004/create_testdata/net.py
@@ -8,13 +8,13 @@ import pandas as pd
 sys.path.insert(1, os.path.join(os.getcwd(), 'mkstd', "examples", "petab_sciml"))
 sys.path.insert(1, os.path.join(os.getcwd(), 'test_cases'))
 from petab_sciml_standard import Input, MLModel, PetabScimlStandard
-from helper import read_array, get_ps_layer, get_dim, extract_numbers
+from helper import read_array, get_ps_layer
 
 class Net(nn.Module):
     def __init__(self) -> None:
         super().__init__()
-        self.layer1 = nn.Conv1d(1, 2, 5)
-        self.layer2 = nn.Conv1d(2, 1, 5)
+        self.layer1 = nn.Conv2d(1, 2, 5)
+        self.layer2 = nn.Conv2d(2, 1, (5, 2))
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         x = self.layer1(input)
@@ -22,7 +22,7 @@ class Net(nn.Module):
         return x
 
 # Create a pytorch module, convert it to PEtab SciML, then save it to disk.
-dir_save = os.path.join(os.getcwd(), 'test_cases', "net_003")
+dir_save = os.path.join(os.getcwd(), 'test_cases', "net_004")
 net = Net()
 mlmodel = MLModel.from_pytorch_module(
     module=net, mlmodel_id="model1", inputs=[Input(input_id="input1")]
@@ -50,4 +50,3 @@ for i in range(1, 4):
     output_ref = read_array(df_output)
     output = net.forward(input)
     torch.testing.assert_close(output_ref, output, atol=1e-3, rtol=0.0)
-
