a7457b14096c0081ede19d753133f04a6f260971
sebapersson <sebastian.persson1@gmail.com>
2024-11-12 14:35:49 +0100

    Refactor PyTorch net

--- src/python/helper.py
+++ src/python/helper.py
@@ -1,4 +1,44 @@
 import torch
+import sys
+import pandas as pd
+import os
+
+sys.path.insert(1, os.path.join(os.getcwd(), 'mkstd', "examples", "petab_sciml"))
+from petab_sciml_standard import Input, MLModel, PetabScimlStandard
+
+def make_yaml(net, dir_save):
+    mlmodel = MLModel.from_pytorch_module(
+    module=net, mlmodel_id="model1", inputs=[Input(input_id="input1")])
+    petab_sciml_mlmodel = PetabScimlStandard.model(models=[mlmodel])
+    PetabScimlStandard.save_data(
+        data=petab_sciml_mlmodel, filename=os.path.join(dir_save, "net.yaml")
+    )
+
+def test_nn(net, dir_save, layer_names, dropout=False, atol=1e-3):
+    for i in range(1, 4):
+        if not layer_names is None:
+            for layer_name in layer_names:
+                df = pd.read_csv(os.path.join(dir_save, "net_ps_" + str(i) + ".tsv"), delimiter='\t')
+                ps_weight = get_ps_layer(df, layer_name, "weight")
+                ps_bias = get_ps_layer(df, layer_name, "bias")
+                with torch.no_grad():
+                    layer = getattr(net, layer_name)
+                    layer.weight[:] = ps_weight
+                    layer.bias[:] = ps_bias
+
+        df_input = pd.read_csv(os.path.join(dir_save, "net_input_" + str(i) + ".tsv"), delimiter='\t')
+        df_output = pd.read_csv(os.path.join(dir_save, "net_output_" + str(i) + ".tsv"), delimiter='\t')
+        input = read_array(df_input)
+        output_ref = read_array(df_output)
+        if dropout == False:    
+            output = net.forward(input)
+        else:
+            output = torch.zeros_like(output_ref)
+            for i in range(40000):
+                output += net.forward(input)
+            output /= 40000
+        torch.testing.assert_close(output_ref, output, atol=atol, rtol=0.0)
+
 
 def extract_numbers(series):
     out = [[int(part) for part in s.split('_') if part] for s in series]
--- test_cases/net_009/create_testdata/net.py
+++ test_cases/net_009/create_testdata/net.py
@@ -1,14 +1,8 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import sys
 import os
-import pandas as pd
 
-sys.path.insert(1, os.path.join(os.getcwd(), 'mkstd', "examples", "petab_sciml"))
-sys.path.insert(1, os.path.join(os.getcwd(), 'test_cases'))
-from petab_sciml_standard import Input, MLModel, PetabScimlStandard
-from helper import read_array, get_ps_layer
+from src.python.helper import make_yaml, test_nn
 
 class Net(nn.Module):
     def __init__(self) -> None:
@@ -22,20 +16,5 @@ class Net(nn.Module):
 # Create a pytorch module, convert it to PEtab SciML, then save it to disk.
 dir_save = os.path.join(os.getcwd(), 'test_cases', "net_009")
 net = Net()
-mlmodel = MLModel.from_pytorch_module(
-    module=net, mlmodel_id="model1", inputs=[Input(input_id="input1")]
-)
-petab_sciml_mlmodel = PetabScimlStandard.model(models=[mlmodel])
-PetabScimlStandard.save_data(
-    data=petab_sciml_mlmodel, filename=os.path.join(dir_save, "net.yaml")
-)
-
-# Test consistency between Lux.jl and PyTorch (a double check for correct values)
-for i in range(1, 4):
-    df_input = pd.read_csv(os.path.join(dir_save, "net_input_" + str(i) + ".tsv"), delimiter='\t')
-    df_output = pd.read_csv(os.path.join(dir_save, "net_output_" + str(i) + ".tsv"), delimiter='\t')
-    input = read_array(df_input)
-    output_ref = read_array(df_output)
-    output = net.forward(input)
-    torch.testing.assert_close(output_ref, output, atol=1e-3, rtol=0.0)
-
+make_yaml(net, dir_save)
+test_nn(net, dir_save, None)
