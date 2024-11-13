994c9add2e6492e1338dcf6644fe59bb9c4c839b
sebapersson <sebastian.persson1@gmail.com>
2024-11-13 09:51:56 +0100

    Update softmax tests

--- test_cases/net_041/create_testdata/net.jl
+++ test_cases/net_041/create_testdata/net.jl
@@ -4,7 +4,7 @@ for i in 1:3
     rng = StableRNG(i)
     ps, st = Lux.setup(rng, nn_model)
     input = rand(rng, 5, 4)
-    output = Lux.logsoftmax(input; dims = 1)
+    output = Lux.logsoftmax(input)
     save_input(joinpath(@__DIR__, ".."), i, input, input_order_jl, input_order_py)
     df_output = _array_to_tidy(output; mapping = [1 => 2, 2 => 1])
     CSV.write(joinpath(@__DIR__, "..", "net_output_$i.tsv"), df_output, delim = '\t')
