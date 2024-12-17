47ec95452d9d4431e8dbc4a1a78ce9da131c6e70
sebapersson <sebastian.persson1@gmail.com>
2024-12-17 16:31:01 +0100

    Update helper functions

--- src/julia/helper.jl
+++ src/julia/helper.jl
@@ -2,7 +2,7 @@
     Helper functions for writing neural-network parameters to file
 =#
 
-using Lux, ComponentArrays, DataFrames, Random
+using Lux, ComponentArrays, DataFrames, Random, HDF5
 using Catalyst: @unpack
 
 function write_yaml(dirsave, input_order_jl, input_order_py, output_order_jl, output_order_py; ps::Bool=true, dropout::Bool=false)::Nothing
@@ -70,26 +70,18 @@ function nn_ps_to_h5(nn, ps::Union{ComponentArray, NamedTuple}, path::String)::N
     return nothing
 end
 
-function set_ps_net!(ps::ComponentArray, df_ps::Nothing, netname::Symbol, nn)::Nothing
-    df_net = df_ps[startswith.( df_ps[!, :parameterId], "$(netname)_"), :]
-    for (id, layer) in pairs(nn.layers)
-        df_layer = df_net[startswith.(df_net[!, :parameterId], "$(netname)_$(id)_"), :]
-        ps_layer = ps[id]
-        set_ps_layer!(ps_layer, layer, df_layer)
-        ps[id] = ps_layer
+function set_ps_net!(ps::ComponentArray, path_h5::String, nn)::Nothing
+    file = h5open(path_h5, "r")
+    for (layerid, layer) in pairs(nn.layers)
+        ps_layer = ps[layerid]
+        isempty(ps_layer) && continue
+        _set_ps_layer!(ps_layer, layer, file[string(layerid)])
+        ps[layerid] = ps_layer
     end
+    close(file)
     return nothing
 end
 
-"""
-    layer_ps_to_h5!(file, layer::Lux.Dense, ps::Union{ComponentArray, NamedTuple}, layername::Symbol)::Nothing
-
-Transforms parameters (`ps`) for a Lux layer into hdf5 format.
-
-For `Dense` layer possible parameters that are saved are:
-- `weight` of dimension `(out_features, in_features)`
-- `bias` of dimension `(out_features)`
-"""
 function layer_ps_to_h5!(file, layer::Lux.Dense, ps::Union{NamedTuple, ComponentArray}, layername::Symbol)::Nothing
     @unpack in_dims, out_dims, use_bias = layer
     g = create_group(file, string(layername))
@@ -97,17 +89,6 @@ function layer_ps_to_h5!(file, layer::Lux.Dense, ps::Union{NamedTuple, Component
     _ps_bias_to_h5!(g, ps, use_bias)
     return nothing
 end
-"""
-    layer_ps_to_h5!(file, layer::Lux.ConvTranspose, ...)::Nothing
-
-For `Conv` layer possible parameters are
-- `weight` of dimension `(in_channels, out_channels, kernel_size)`
-- `bias` of dimension `(out_channels)`
-
-!!! note
-    Note, in Lux.jl `weight` has `(kernel_size, in_channels, out_channels)`. This is fixed
-    by the importer.
-"""
 function layer_ps_to_h5!(file, layer::Lux.Conv, ps::Union{NamedTuple, ComponentArray}, layername::Symbol)::Nothing
     @unpack kernel_size, use_bias, in_chs, out_chs = layer
     if length(kernel_size) == 1
@@ -135,17 +116,6 @@ function layer_ps_to_h5!(file, layer::Lux.Conv, ps::Union{NamedTuple, ComponentA
     _ps_bias_to_h5!(g, ps, use_bias)
     return nothing
 end
-"""
-    layer_ps_to_h5!(file, layer::Lux.ConvTranspose, ...)::Nothing
-
-For `ConvTranspose` layer possible parameters that are saved to a DataFrame are:
-- `weight` of dimension `(in_channels, out_channels, kernel_size)`
-- `bias` of dimension `(out_channels)`
-
-!!! note
-    Note, in Lux.jl `weight` has `(kernel_size, out_channels, in_channels)`. This is fixed
-    by the importer.
-"""
 function layer_ps_to_h5!(file, layer::Lux.ConvTranspose, ps::Union{NamedTuple, ComponentArray}, layername::Symbol)::Nothing
     @unpack kernel_size, use_bias, in_chs, out_chs = layer
     if length(kernel_size) == 1
@@ -163,13 +133,6 @@ function layer_ps_to_h5!(file, layer::Lux.ConvTranspose, ps::Union{NamedTuple, C
     _ps_bias_to_h5!(g, ps, use_bias)
     return nothing
 end
-"""
-    layer_ps_to_h5!(file, layer::Lux.Bilinear, ...)::Nothing
-
-For `Bilinear` layer possible parameters that are saved are:
-- `weight` of dimension `(out_features, in_features1, in_features2)`
-- `bias` of dimension `(out_features)`
-"""
 function layer_ps_to_h5!(file, layer::Lux.Bilinear, ps::Union{NamedTuple, ComponentArray}, layername::Symbol)::Nothing
     @unpack in1_dims, in2_dims, out_dims, use_bias = layer
     g = create_group(file, string(layername))
@@ -177,15 +140,6 @@ function layer_ps_to_h5!(file, layer::Lux.Bilinear, ps::Union{NamedTuple, Compon
     _ps_bias_to_h5!(g, ps, use_bias)
     return nothing
 end
-"""
-    layer_ps_to_h5!(file, layer::Union{Lux.BatchNorm, Lux.InstanceNorm}, ...)::Nothing
-
-For `BatchNorm` and `InstanceNorm` layer possible parameters that are saved are:
-- `scale/weight` of dimension `(num_features)`
-- `bias` of dimension `(num_features)`
-!!! note
-    in Lux.jl the dimension argument `num_features` is chs (number of input channels)
-"""
 function layer_ps_to_h5!(file, layer::Union{Lux.BatchNorm, Lux.InstanceNorm}, ps::Union{NamedTuple, ComponentArray}, layername::Symbol)::Nothing
     @unpack affine, chs = layer
     affine == false && return nothing
@@ -194,19 +148,6 @@ function layer_ps_to_h5!(file, layer::Union{Lux.BatchNorm, Lux.InstanceNorm}, ps
     _ps_bias_to_h5!(g, ps, true)
     return nothing
 end
-"""
-    layer_ps_to_h5!(file, layer::Lux.LayerNorm, ...)::Nothing
-
-For `LayerNorm` layer possible parameters that are saved to a DataFrame are:
-- `scale/weight` of `size(input)` dimension
-- `bias` of `size(input)` dimension
-!!! note
-    Input order differs between Lux.jl and PyTorch. Order `["C", "D", "H", "W"]` in
-    PyTorch corresponds to `["W", "H", "D", "C"]` in Lux.jl. Basically, regardless of input
-    dimension the Lux.jl dimension is the PyTorch dimension reversed.
-!!! note
-    In Lux.jl the input dimension in `size(input, 1)`.
-"""
 function layer_ps_to_h5!(file, layer::LayerNorm, ps::Union{NamedTuple, ComponentArray}, layername::Symbol)::Nothing
     @unpack shape, affine = layer
     affine == false && return DataFrame()
@@ -229,25 +170,26 @@ function layer_ps_to_h5!(file, layer::LayerNorm, ps::Union{NamedTuple, Component
     _ps_bias_to_h5!(g, _ps, true)
     return nothing
 end
-"""
-    layer_ps_to_h5!(file, layer::Union{Lux.MaxPool, Lux.FlattenLayer}, ...)::Nothing
-
-Pooling layers do not have parameters.
-"""
 function layer_ps_to_h5!(file, layer::Union{Lux.MaxPool, Lux.MeanPool, Lux.LPPool, Lux.AdaptiveMaxPool, Lux.AdaptiveMeanPool, Lux.FlattenLayer, Lux.Dropout, Lux.AlphaDropout}, ::Union{NamedTuple, ComponentArray, Vector{<:AbstractFloat}}, ::Symbol)::Nothing
     return nothing
 end
 
-function set_ps_layer!(ps::ComponentArray, layer::Lux.Dense, df_ps::Nothing)::Nothing
+function _set_ps_layer!(ps::ComponentArray, layer::Lux.Dense, file_group)::Nothing
     @unpack in_dims, out_dims, use_bias = layer
-    _set_ps_weight!(ps, (out_dims, in_dims), df_ps)
-    _set_ps_bias!(ps, (out_dims, ), df_ps, use_bias)
+    @assert size(ps.weight) == (out_dims, in_dims) "Error in dimension of weights for Dense layer"
+    ps_weight = _get_ps_layer(file_group, :weight)
+    @views ps.weight .= ps_weight
+
+    use_bias == false && return nothing
+    @assert size(ps.bias) == (out_dims, ) "Error in dimension of bias for Dense layer"
+    ps_bias = _get_ps_layer(file_group, :bias)
+    @views ps.bias .= ps_bias
     return nothing
 end
-function set_ps_layer!(ps::ComponentArray, layer::Lux.Conv, df_ps::Nothing)::Nothing
+function _set_ps_layer!(ps::ComponentArray, layer::Lux.Conv, file_group)::Nothing
     @unpack kernel_size, use_bias, in_chs, out_chs = layer
     @assert size(ps.weight) == (kernel_size..., in_chs, out_chs) "Error in dimension of weights for Conv layer"
-    _ps_weight = _get_ps_layer(df_ps, length((kernel_size..., in_chs, out_chs)), :weight)
+    _ps_weight = _get_ps_layer(file_group, :weight)
     if length(kernel_size) == 1
         ps_weight = _reshape_array(_ps_weight, [1 => 3, 2 => 2, 3 => 1])
     elseif length(kernel_size) == 2
@@ -259,35 +201,19 @@ function set_ps_layer!(ps::ComponentArray, layer::Lux.Conv, df_ps::Nothing)::Not
 
     use_bias == false && return nothing
     @assert size(ps.bias) == (out_chs, ) "Error in dimension of bias for Conv layer"
-    ps_bias = _get_ps_layer(df_ps, 1, :bias)
+    ps_bias = _get_ps_layer(file_group, :bias)
     @views ps.bias .= ps_bias
     return nothing
 end
-function set_ps_layer!(::Union{ComponentArray, Vector{<:AbstractFloat}}, ::Union{Lux.MaxPool, Lux.FlattenLayer}, ::Nothing)::Nothing
-    return nothing
-end
 
-function _set_ps_weight!(ps::ComponentArray, weight_dims, df_ps::Nothing)::Nothing
-    @assert size(ps.weight) == weight_dims "layer size does not match ps.weight size"
-    df_weights = df_ps[occursin.("weight_", df_ps[!, :parameterId]), :]
-    for (i, id) in pairs(df_weights[!, :parameterId])
-        _ix = parse.(Int64, collect(m.match for m in eachmatch(r"\d+", id))[(end-length(weight_dims)+1):end])
-        ix = [x + 1 for x in _ix]
-        ps.weight[ix...] = df_weights[i, :value]
+function _get_ps_layer(file_group, which::Symbol)
+    if which == :weight
+        ps = HDF5.read_dataset(file_group, "weight")
+    else
+        ps = HDF5.read_dataset(file_group, "bias")
     end
-    return nothing
-end
-
-function _set_ps_bias!(ps::ComponentArray, bias_dims, df_ps::Nothing, use_bias)::Nothing
-    use_bias == false && return nothing
-    @assert size(ps.bias) == bias_dims "layer size does not match ps.bias size"
-    df_bias = df_ps[occursin.("bias_", df_ps[!, :parameterId]), :]
-    for (i, id) in pairs(df_bias[!, :parameterId])
-        _ix = parse.(Int64, collect(m.match for m in eachmatch(r"\d+", id))[(end-length(bias_dims)+1):end])
-        ix = [x + 1 for x in _ix]
-        ps.bias[ix...] = df_bias[i, :value]
-    end
-    return nothing
+    # Julia is column-major, while the standard format is row-major
+    return permutedims(ps, reverse(1:ndims(ps)))
 end
 
 function _ps_weight_to_h5!(g, ps; scale::Bool = false)::Nothing
@@ -325,24 +251,4 @@ function _reshape_array(x, mapping)
         xout[inew...] = x[i]
     end
     return xout
-end
-
-function _get_ps_layer(df_layer::Nothing, lendim::Int64, which::Symbol)
-    if which == :weight
-        df = df_layer[occursin.("weight_", df_layer[!, :parameterId]), :]
-    else
-        df = df_layer[occursin.("bias_", df_layer[!, :parameterId]), :]
-    end
-    ix = Any[]
-    for pid in df[!, :parameterId]
-        _ix = parse.(Int64, collect(m.match for m in eachmatch(r"\d+", pid))[((end-lendim+1):end)])
-        # Python -> Julia indexing
-        _ix .+= 1
-        push!(ix, Tuple(_ix))
-    end
-    out = zeros(maximum(ix))
-    for i in eachindex(ix)
-        out[ix[i]...] = df[i, :value]
-    end
-    return out
 end
